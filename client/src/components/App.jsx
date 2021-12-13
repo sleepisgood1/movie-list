11ba786e99cee373a18073d8a756d61733e560d8
sleepisgood1 <jae1994krw@gmail.com>
2021-12-12 16:15:44 -0800

    completed up to level 1

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,62 @@
 import React from 'react';
+import MovieList from './MovieList.jsx'
+import SearchForm from './SearchForm.jsx'
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isSearched: false,
+      movies: [
+        {title: 'Mean Girls'},
+        {title: 'Hackers'},
+        {title: 'The Grey'},
+        {title: 'Sunshine'},
+        {title: 'Ex Machina'},
+      ],
+      filteredMovies: []
+    }
+    this.onSubmitOfSearch = this.onSubmitOfSearch.bind(this)
+  }
+  onSubmitOfSearch(input) {
+    console.log(input)
+    // event.preventDefault();
+    var filteredMovie = this.matchMovie(this.state.movies, input)
+    this.setState({
+      isSearched: !this.state.isSearched,
+      filteredMovies: filteredMovie
+    })
+  }
+  matchMovie(movies, input) {
+    var matchedMovies = [];
+      for (let i=0;i<movies.length;i++) {
+        if (movies[i]["title"].toLowerCase().indexOf(input.toLowerCase()) !== -1) {
+          matchedMovies.push(movies[i])
+        }
+      }
+      return matchedMovies
+  }
+
+  render () {
+    if (this.state.isSearched) {
+      return(
+        <div>
+          <SearchForm />
+          <MovieList allMovies={this.state.filteredMovies}/>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <SearchForm search={this.onSubmitOfSearch}/>
+          <MovieList allMovies={this.state.movies}/>
+        </div>
+      )
+    }
+  }
+}
+// const App = (props) => (
+//   <div>Hello World!</div>
+// );
 
 export default App;
